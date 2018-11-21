6557a2e81a770c7c74a168bb2bb7368686119860
zxn359014360 <359014360@qq.com>
2018-11-21 15:29:25 +0800

    集成redis，rsa加密解密，sha256.js加密

--- src/main/webapp/resources/js/secretjs/rsa/bodyRsa.js
+++ src/main/webapp/resources/js/secretjs/rsa/bodyRsa.js
@@ -3,7 +3,7 @@ function encrypt(string) {
     //实例化加密对象
     var encrypt = new JSEncrypt();
     //设置加密公钥
-    encrypt.setPublicKey("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCE0wojG1fnUtJs/MYtTG6xqq9yE1vucbNV9JhTAosHP15z8DokLnPXTPs/fGXWDs3k0S9uZmQ/tQBHUKrn+reKL/I6r+86v1aUtaeL7FmirNpLFhkVRKCch81Kk2w9ePd/MRAjV4Y8osuBXEZVAls7G4I+5Nv4vV3InmT6PpAd7QIDAQAB");
+    encrypt.setPublicKey("");
 
     //分段加密
     var encryptResult =encrypt.encrypt(string);
